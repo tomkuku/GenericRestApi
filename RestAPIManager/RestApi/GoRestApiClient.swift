ed7defc05266afaae2cd640a7fe5da23ea77601b
tkukulka <61714983+tomkuku@users.noreply.github.com>
2022-01-21 17:53:41 +0100

    Created UpdateUserGoRestApiRequest and tests

--- RestAPIManager/RestApi/GoRestApiClient.swift
+++ RestAPIManager/RestApi/GoRestApiClient.swift
@@ -7,37 +7,8 @@
 
 import Foundation
 
-enum GoRestGetUsersRequestError: Error {
-    case server
-    case client
-    case other
-}
-
-enum GoRestAddUserRequestError: Error {
-    case nameTaken
-    case emailTaken
-    case server
-    case client
-}
-
-enum GoRestUpdateUserRequestError: Error {
-    case invalidUserId
-    case nameTaken
-    case emailTaken
-    case server
-    case client
-}
-
-enum GoRestDeleteUserRequestError: Error {
-    case invalidUserId
-    case server
-    case client
-}
-
 protocol GoRestApiClient {
     func call<R: HTTPRequest>(request: R, completion: @escaping (R.ResultType) -> Void)
-    
-    func updateUser(_ user: User, completion: @escaping (Result<Void, GoRestUpdateUserRequestError>) -> Void)
 }
 
 final class GoRestApiClientImpl: GoRestApiClient {
@@ -46,47 +17,6 @@ final class GoRestApiClientImpl: GoRestApiClient {
     
     init(httpClient: HTTPClient = HTTPClientImpl()) {
         self.httpClient = httpClient
-    }
-    
-    func updateUser(_ user: User, completion: @escaping (Result<Void, GoRestUpdateUserRequestError>) -> Void) {
-        DispatchQueue.global().async {
-            var urlComponents = URLComponents()
-            urlComponents.scheme = "https"
-            urlComponents.host = "gorest.co.in"
-            urlComponents.path = "/public/v1/users/\(user.id!)"
-            
-            var request = URLRequest(url: urlComponents.url!)
-            request.httpMethod = HTTPMethod.patch.rawValue
-            request.allHTTPHeaderFields = [
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization": "Bearer \(Config.apiKey)"
-            ]
-                
-            self.httpClient.request(request) { result in
-                if case .success(let response) = result {
-                    switch response.statusCode! {
-                    case 200:
-                        completion(.success(()))
-                        
-                    case 404:
-                        completion(.failure(.invalidUserId))
-                        
-                    case 422:
-                        // parsing here...
-                        completion(.failure(.emailTaken))
-                        
-                    case 400...499:
-                        completion(.failure(.client))
-                        
-                    case 500...599:
-                        completion(.failure(.server))
-                        
-                    default: break
-                    }
-                }
-            }
-        }
     }
     
     func call<R: HTTPRequest>(request: R, completion: @escaping (R.ResultType) -> Void) {
